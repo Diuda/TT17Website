152e5a74b770716d158467230ecf1b91371adce1
Diuda <divyanshu.1996@gmail.com>
2017-07-19 01:14:19 +0530

    map coming up size not right

--- loading.js
+++ loading.js
@@ -3,7 +3,7 @@ var loadingState = new Kiwi.State('LoadingState');
 loadingState.preload = function(){
 
 	Kiwi.State.prototype.preload.call( this );
-	this.addImage('map', './MIT_map.png', 0, 0, true);
+	this.addImage('map', './MIT_map.png');
 	this.addSpriteSheet('characterSprite', './rocket.png', 490, 500, true);
 };
 
@@ -11,15 +11,8 @@ loadingState.create = function(){
 
 
 	Kiwi.State.prototype.create.call( this );
-	this.map = new Kiwi.GameObjects.Sprite(this, this.textures.map, 0, 0);
-	this.addChild(this.map);
+	// this.map = new Kiwi.GameObjects.StaticImage(this, this.textures.map, 0, 0);
+	// this.addChild(this.map);
 	this.game.states.switchState("state1");
 };
 
-// var gameOptions = {
-// 	renderer: Kiwi.RENDERER_CANVAS,
-// 	width: 3263,
-// 	height: 4867	
-// };
-
-// var game = new Kiwi.Game('game-container', 'LoadingAnImage', state, gameOptions);
--- state1.js
+++ state1.js
@@ -2,6 +2,9 @@ var state1 = new Kiwi.State( "state1" );
 
 state1.create = function() {
 	Kiwi.State.prototype.create.call( this );
+
+	this.background = new Kiwi.GameObjects.StaticImage(
+		this, this.textures[ "map" ], 0, 0 );
 	this.character = new Kiwi.GameObjects.Sprite(
 		this, this.textures[ "characterSprite" ], 16, 291 );
 
@@ -15,15 +18,7 @@ state1.create = function() {
 		Kiwi.Input.Keycodes.DOWN );
 
 	this.character.animation.add(
-		"idleright", [ 0 ], 0.1, false );
-	this.character.animation.add(
-		"crouchright", [ 1 ], 0.1, false );
-	this.character.animation.add(
 		"moveright", [ 2, 3, 4, 5, 6, 7 ], 0.1, true );
-	this.character.animation.add(
-		"idleleft", [ 8 ], 0.1, false );
-	this.character.animation.add(
-		"crouchleft", [ 9 ], 0.1, false );
 	this.character.animation.add(
 		"moveleft", [ 10, 11, 12, 13, 14, 15 ], 0.1, true );
 	this.character.animation.add(
@@ -31,21 +26,19 @@ state1.create = function() {
 	this.character.animation.add(
 		"movedown", [ 22, 23, 24, 25, 26, 27 ], 0.1, true );
 
-	this.facing = "right";
+	// this.facing = "right";
 
-	this.character.animation.play( "idleright" );
+	// this.character.animation.play( "idleright" );
 
+	this.addChild(this.background);
 	this.addChild( this.character );
+
 };
 
 
 state1.update = function() {
 	Kiwi.State.prototype.update.call( this );
 
-	// if ( this.stateKey.isDown && this.character.transform.x < 60 ) {
-
-	// 	// this.game.states.switchState( "OutsideState" );
-	// }
 
 	if ( this.downKey.isDown ) {
 		if ( this.character.transform.y > 3 ) {
@@ -66,7 +59,6 @@ state1.update = function() {
 	}
 	else if ( this.leftKey.isDown ) {
 
-		this.facing = "left";
 		if ( this.character.transform.x > 3 ) {
 			this.character.transform.x -= 3;
 		}
@@ -76,7 +68,6 @@ state1.update = function() {
 	}
 	else if ( this.rightKey.isDown ) {
 
-		this.facing = "right";
 		if ( this.character.transform.x < 600 ) {
 			this.character.transform.x += 3;
 		}
